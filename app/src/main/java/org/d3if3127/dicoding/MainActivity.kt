5a8b9b3df5791981772091f461db5e5913c3f672
FadlyRamdhani4503 <91235752+FadlyRamdhani4503@users.noreply.github.com>
2023-09-04 14:17:54 +0700

    Btn Pindah Activity

--- app/src/main/java/org/d3if3127/dicoding/MainActivity.kt
+++ app/src/main/java/org/d3if3127/dicoding/MainActivity.kt
@@ -1,11 +1,27 @@
 package org.d3if3127.dicoding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    val btnMove: Button = findViewById(R.id.btn_move_activity)
+        btnMove.setOnClickListener(this)
+
+    }
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/org/d3if3127/dicoding/MoveActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if3127.dicoding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
